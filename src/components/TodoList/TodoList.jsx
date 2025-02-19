e07a2c605f3ff7ad18bea7ce386b4dbcbf4287fc
gys <gys@ibdata.co.kr>
2025-02-19 17:30:33 +0900

    다크모드

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,21 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
+import { v4 as uuidv4 } from 'uuid';
+import styles from './TodoList.module.css'
 
-export default function TodoList() {
-    const [todos, setTodos] = useState([
-        { id: '123', text: '장보기', status: 'active' },
-        { id: '124', text: '공부하기', status: 'active' },
-    ])
+export default function TodoList({ filter }) {
+    const [todos, setTodos] = useState(readTodosFromLocalStorage);
 
+    const handleAdd = (todo) => {
+        setTodos([...todos, todo]);
+    }
+
+    const handleUpdate = (updated) => {
+        setTodos(todos.map(todo => todo.id === updated.id ? updated : todo));
+    }
+
+    const handleDelete = (deleted) => {
+        setTodos(todos.filter(todo => todo.id !== deleted.id));
+    }
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    const filtered = getFilteredItems(todos, filter);
     return (
-        <section>
-            <ul>
-                {
-                    todos.map((item) => (
-                        <li key={item.id}>{item.text}</li>
-                    ))
-                }
+        <section className={styles.container}>
+            <ul className={styles.list}>
+                {filtered.map((item) => (
+                    <Todo 
+                        key={item.id} 
+                        todo={item} 
+                        onUpdate={handleUpdate} 
+                        onDelete={handleDelete} 
+                    />
+                ))}
             </ul>
+            <AddTodo onAdd={handleAdd} />
         </section>
     );
 }
 
+function readTodosFromLocalStorage() {
+    const todos = localStorage.getItem('todos');
+    return todos ? JSON.parse(todos) : [];
+}
+
+function getFilteredItems(todos, filter) {
+    if(filter === 'all') {
+        return todos;
+    }
+    return todos.filter((todo) => todo.status === filter);
+}
